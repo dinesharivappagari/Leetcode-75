f3d82c272588cfedbf8676b2484e357b0e1aa205
SAIDINESH A <saidinesh2662@gmail.com>
2024-07-19 18:58:29 +0530

    Delete gcdof2str.py

--- gcdof2str.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def gcdOfstrings(self, str1: str, str2: str) -> str:
-        if str1 + str2 != str2 + str1:
-            return ""
-        from math import gcd
-        return str1[:gcd(len(str1), len(str2))]
